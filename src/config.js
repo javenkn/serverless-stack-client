ad4d2f5ec57a614f3638c31f7adf3d90ef70bee2
javenkn <javenkn7@gmail.com>
2019-10-23 12:43:48 -1000

    Sets up AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-fileuploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://6ew7p6hkn3.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_jyEBdzRsD",
+    APP_CLIENT_ID: "3fc6plc9md2gfp6r883ttl35nr",
+    IDENTITY_POOL_ID: "us-east-1:a9a2d2df-5b3d-41d2-90d4-21e7856ad08f"
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import Amplify from "aws-amplify";
+import config from "./config";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+Amplify.configure({
+  // AWS resources that we want to interact with
+  Auth: {
+    // Cognito
+    mandatorySignIn: true, // must sign in
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    // S3
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    // API Gateway
+    endpoints: [
+      {
+        name: "notes", // name of the api
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      }
+    ]
+  }
+});
 
 ReactDOM.render(
   <BrowserRouter>
