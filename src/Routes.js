ee5ca79f4be8d40c6576fae4dc894a76b63f58a6
javenkn <javenkn7@gmail.com>
2019-10-23 19:29:12 -1000

    Fixes typo

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,14 @@ import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
 import Settings from "./containers/Settings";
+import AppliedRoute from "./components/AuthenticatedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <UnauthenticatedRoute
         path="/login"
         exact
